c16304607e0d6cfe368f76c4f5db7b51b7368ee0
wangyanyi <wyysunnyboy@163.com>
2022-06-15 17:37:24 +0800

    在排序数组中查找元素的第一个和最后一个位置02

--- leetcode/0034.Find-the-first-and-last/Find-the-first-and-last.go
+++ leetcode/0034.Find-the-first-and-last/Find-the-first-and-last.go
@@ -44,3 +44,43 @@ func searchRange(nums []int, target int) []int {
 
 	return []int{-1, -1}
 }
+
+func searchRange2(nums []int, target int) []int {
+	getLeft := func(nums []int, target int) int {
+		left, right := 0, len(nums)-1
+		for left <= right {
+			mid := left + (right-left)>>1
+			if nums[mid] > target {
+				right = mid - 1
+			} else if nums[mid] < target {
+				left = mid + 1
+			} else {
+				if mid == 0 || nums[mid-1] != target {
+					return mid
+				}
+				right = mid - 1
+			}
+		}
+		return -1
+	}
+
+	getRight := func(nums []int, target int) int {
+		left, right := 0, len(nums)-1
+		for left <= right {
+			mid := left + (right-left)>>1
+			if nums[mid] > target {
+				right = mid - 1
+			} else if nums[mid] < target {
+				left = mid + 1
+			} else {
+				if mid == len(nums)-1 || nums[mid+1] != target {
+					return mid
+				}
+				left = mid + 1
+			}
+		}
+		return -1
+	}
+
+	return []int{getLeft(nums, target), getRight(nums, target)}
+}
--- leetcode/0034.Find-the-first-and-last/Find-the-first-and-last_test.go
+++ leetcode/0034.Find-the-first-and-last/Find-the-first-and-last_test.go
@@ -18,11 +18,36 @@ func Test_searchRange(t *testing.T) {
 		{"01", args{nums: []int{5, 7, 7, 8, 8, 10}, target: 8}, []int{3, 4}},
 		{"02", args{nums: []int{5, 7, 7, 8, 8, 10}, target: 6}, []int{-1, -1}},
 		{"03", args{nums: []int{}, target: 0}, []int{-1, -1}},
+		{"04", args{nums: []int{2, 2}, target: 2}, []int{0, 1}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := searchRange(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("searchRange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_searchRange2(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{"01", args{nums: []int{5, 7, 7, 8, 8, 10}, target: 8}, []int{3, 4}},
+		{"02", args{nums: []int{5, 7, 7, 8, 8, 10}, target: 6}, []int{-1, -1}},
+		{"03", args{nums: []int{}, target: 0}, []int{-1, -1}},
+		{"04", args{nums: []int{2, 2}, target: 2}, []int{0, 1}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := searchRange2(tt.args.nums, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("searchRange2() = %v, want %v", got, tt.want)
 			}
 		})
 	}
