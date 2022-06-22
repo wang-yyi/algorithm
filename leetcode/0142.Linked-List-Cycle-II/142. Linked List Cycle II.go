77458aef87bcbc6882da703898e11ae97f6e6953
wangyanyi <wyysunnyboy@163.com>
2022-06-22 17:34:53 +0800

    检查链表是否有环

---
+++ leetcode/0142.Linked-List-Cycle-II/142. Linked List Cycle II.go
@@ -0,0 +1,30 @@
+package leetcode
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			for slow != head {
+				slow = slow.Next
+				head = head.Next
+			}
+			return head
+		}
+	}
+	return nil
+}
